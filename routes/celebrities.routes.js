3c973bdd9c8580f3f8bf0f1cd63077f091d0173a
Ralf Bonnick <ralfbonnick@gmx.net>
2022-10-31 18:14:31 +0100

    Iteration3 to 80% done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -6,9 +6,22 @@ router.get("/", (req, res, next) => {
     res.render("celebrities/celebrities");
   });
 
-router.get("/new", (req, res, next) => {
+router.get("/create", (req, res, next) => {
   res.render("celebrities/new-celebrity");
 });
+
+router.post("/create", async (req, res, next) => {
+  try {
+    await celebritiesDB.create({
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase,
+    })
+    res.redirect('/celebrities/create')
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 module.exports = router
 
